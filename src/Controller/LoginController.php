506e9cfbaffb8791f5416ce7400bf4b1231e310b
Saflou <dz-siross@hotmail.fr>
2024-01-23 14:01:40 +0100

    fix login cache token

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -14,21 +14,24 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        // Créer et gérer le formulaire de connexion
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home'); 
+        }
+        // // Créer et gérer le formulaire de connexion
         $form = $this->createForm(LoginFormType::class);
-        $form->handleRequest($request);
+        // $form->handleRequest($request);
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastEmail = $authenticationUtils->getLastUsername();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            return $this->redirectToRoute('app_home'); 
-        }
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     return $this->redirectToRoute('app_home'); 
+        // }
 
         // Rendre la vue avec le formulaire et les éventuelles erreurs d'authentification
         return $this->render('login/index.html.twig', [
             'form' => $form->createView(),
-            'last_email' => $lastEmail,
+            'last_username' => $lastEmail,
             'error' => $error,
         ]);
     }
